34f9bf97151131ed7a86798db8860475f73dab6f
rbnnovais <rbnnovais@gmail.com>
2024-12-04 19:27:34 -0300

    feat: criação de novas tabelas no banco de dados, controllers, models e routes
    nesse novo patch foi realizado a criação de novas tabelas, controllers, models e routes para a estrutura da livraria.

--- LivrariaBackEnd/src/controllers/userController.ts
+++ LivrariaBackEnd/src/controllers/userController.ts
@@ -1,15 +1,31 @@
 import { Request, Response } from 'express';
-import { users, User } from '../models/userModel';
+import { UserService } from '../services/userService';
+import { hashPassword, comparePassword } from '../helpers/hashHelper';
 
-export const getUsers = (req: Request, res: Response): void => {
-  res.json(users);
+const userService = new UserService();
+
+export const getUsers = async (_req: Request, res: Response) => {
+  try {
+    const users = await userService.listUsers();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
 };
 
-export const createUser = (req: Request, res: Response): void => {
-  const newUser: User = {
-    id: users.length + 1,
-    ...req.body
-  };
-  users.push(newUser);
-  res.status(201).json(newUser);
+export const addUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const passwordhash = hashPassword(password);
+    const user = await userService.createUser(name, email, passwordhash);
+    res.status(201).json(user);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.status(400).json({ error: 'Erro desconhecido' });
+    }
+  }
 };
